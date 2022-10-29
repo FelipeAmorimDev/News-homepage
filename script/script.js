9573446323de116414f34f96e11e066f17ce2408
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-10-29 16:33:19 -0300

    Melhorado o tooglejs

--- script/script.js
+++ script/script.js
@@ -1,14 +1,18 @@
 const menuBt = document.getElementById("menuhb");
 const nav = document.querySelector(".nav")
+
 function toogleMenu(){
   const ativo = nav.classList.contains("show")
   if(!ativo){
     nav.classList.add("show")
     menuBt.setAttribute("src","./assets/images/icon-menu-close.svg")
+    document.body.style.overflow = "hidden"
+
   }
   else{
     nav.classList.remove("show")
     menuBt.setAttribute("src","./assets/images/icon-menu.svg")
+    document.body.style.overflow = "visible"
   }
 }
 
